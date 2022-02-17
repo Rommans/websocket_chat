88fc7b5109553c5a6b1779546e3973040ab5ef9d
Rommans <rkokotko3@gmail.com>
2022-02-17 22:20:50 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # websocket_chat
+# Run the WebSocket server via command: npm run start
+# After that server will be available on localhost:8080
---
+++ client/chat.js
@@ -0,0 +1,60 @@
+const connection = new WebSocket('ws://localhost:8080');
+let username;
+
+connection.onopen = () => {
+    console.log('connected');
+    username = prompt('Input your name', 'User');
+};
+
+connection.onclose = () => {
+    console.error('disconnected');
+};
+
+connection.onerror = error => {
+    console.error('failed to connect', error);
+};
+
+connection.onmessage = event => {
+    let obj = JSON.parse(event.data)
+    let messageContainer = document.querySelector('#chat');
+    let el = document.createElement('div');
+    el.setAttribute('class', 'other-message_block');
+    el.innerHTML = `
+        <label>${obj.username}:</label>
+        <p class="other-message">${obj.message}</p>
+        <label class="time">${obj.date}</label>
+    `;
+    messageContainer.appendChild(el);
+};
+
+document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault();
+    let message = document.querySelector('#message').value;
+    if(message.length > 0){
+        let time = new Date().toLocaleTimeString([], {
+            hour12: false
+        });
+    
+        let msg = {
+            username: username,
+            message: message,
+            date: time
+        };
+        connection.send(JSON.stringify(msg));
+        ownMessage(message)
+        document.querySelector('#message').value = '';
+    }
+});
+
+const ownMessage = (message) => {
+    let time = new Date().toLocaleTimeString([], { hour12: false });
+    let messageContainer = document.querySelector('#chat');
+    let el = document.createElement('div');
+    el.setAttribute('class', 'my-message_block');
+    el.innerHTML = `
+          <label>${username}:</label>
+          <p class="my-message">${message}</p>
+          <label class="time">${time}</label>
+      `;
+    messageContainer.appendChild(el);
+}
